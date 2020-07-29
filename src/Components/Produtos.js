5bbc17900bdabbc956f52a76124505293d7eb5b8
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-29 15:08:35 -0300

    commit inicial

---
+++ src/Components/Produtos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Produto.modules.css'
+
+
+function Produtos() {
+    return (
+        <div>
+            <h1>Produtos</h1>
+        </div>
+    )
+}
+
+export default Produtos
