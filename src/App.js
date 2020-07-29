30aa1a05067cb9f9358a7a5a194f53ed8d837ef5
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-29 15:46:19 -0300

    Contruido alguns componentes básicos e o uso do style-module é o destaque. Eu honestamente ainda não vi vantagem aprox 14 minutos da primeira aula do desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
 import Produtos from './Components/Produtos'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Contato from './Components/Contato'
+
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
       
       <BrowserRouter>
+      <Header />
+      <div className="content">
         <Routes>
-          <Route path="/" element={<Produtos />}></Route>
-            
+          <Route path="/" element={<Produtos />}></Route> 
+          <Route path="contato" element={<Contato />}></Route>               
         </Routes>
-
+      </div>
+      <Footer />
       </BrowserRouter>
-     
-
-
-      
     </div>
   )
 }
 
 export default App
+
+     
+
+
+      
---
+++ src/Components/Contato.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Contato.module.css'
+
+function Contato() {
+    return (
+        <div>
+            <h1>Contato</h1>
+        </div>
+    )
+}
+
+export default Contato
