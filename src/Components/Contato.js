a52ad123db64daa2425dfeb1debdcccea244f237
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-29 16:30:42 -0300

    Aqui a criação do Head, que muda o titulo da pagina dependendo do componente que se apresenta. Acho relevante essa parte por isso fiz esse comit. Ele está no final da primeira aula

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Contato.module.css'
 import foto from '../img/contato.jpg'
+import Head from './Head'
 
 function Contato() {
     return (
         <section className={`${styles.contato} animeLeft`}>
+            <Head title="Ranek | Contato" description="Entre em contato" />
             <img src={foto} alt="maquina de escrever"/>
             <div>
             <h1>Entre em contato</h1>
