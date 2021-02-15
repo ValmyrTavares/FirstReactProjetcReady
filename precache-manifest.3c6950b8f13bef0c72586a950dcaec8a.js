df4d5227c2181192ee5a0a0037329e03d3155a8b
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-02-15 15:54:12 -0300

    Updates

---
+++ precache-manifest.3c6950b8f13bef0c72586a950dcaec8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624eb7e495c476b9b0f6b580b85edc17",
+    "url": "/FirstDeployInGitHubPage/index.html"
+  },
+  {
+    "revision": "ee72f4e895ef59d63a76",
+    "url": "/FirstDeployInGitHubPage/static/css/main.54486906.chunk.css"
+  },
+  {
+    "revision": "41189673f9ac9837786b",
+    "url": "/FirstDeployInGitHubPage/static/js/2.220dd62e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstDeployInGitHubPage/static/js/2.220dd62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee72f4e895ef59d63a76",
+    "url": "/FirstDeployInGitHubPage/static/js/main.12b212ae.chunk.js"
+  },
+  {
+    "revision": "4263d830fde0bb97fd6a",
+    "url": "/FirstDeployInGitHubPage/static/js/runtime-main.54b816ec.js"
+  },
+  {
+    "revision": "20d1d7bb569e9158b47473eba9c4ca2c",
+    "url": "/FirstDeployInGitHubPage/static/media/contato.20d1d7bb.jpg"
+  }
+]);
